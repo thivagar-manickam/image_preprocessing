096125d070cbfbd2f6a3609b121ec7e8853ac76f
thivagar-manickam <thivagar200@gmail.com>
2023-07-22 18:11:19 +0530

    Canny edge detection implementation

--- basic_image_preprocessing/image_enhancement/conventional/conventional.py
+++ basic_image_preprocessing/image_enhancement/conventional/conventional.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 from warnings import filterwarnings
 from basic_image_preprocessing.exception.custom_exception import CustomException
-from basic_image_preprocessing.utility.utlity import load_image, plot_graph, validate_channel_param, validate_cmap_value
+from basic_image_preprocessing.utility.utlity import load_image, plot_graph, validate_channel_param, \
+    validate_cmap_value, validate_param_type
 from typing import List, Union
 
 filterwarnings('ignore')
@@ -128,7 +129,7 @@ class ConventionalImageEnhancement:
 
         """
         This definition will apply the CLAHE transformation on the given image
-        with the value and method given in the parameters
+        with the defined parameter values
 
         Input:
             clip_value -> This parameter sets the threshold for contrast limiting. It is the contrast limit for
@@ -166,9 +167,9 @@ class ConventionalImageEnhancement:
                 Accepted value -> [0, 1, 2]
 
         Output:
-            numpy.ndarray -> image post applying the equalization equation on the given image
+            numpy.ndarray -> image post applying the CLAHE transformation on the given image
         """
-        
+
         try:
             validate_cmap_value(cmap, 'CLAHE', 'cmap')
 
@@ -246,3 +247,65 @@ class ConventionalImageEnhancement:
             raise Exception(f"An error occurred while trying to apply the CLAHE transformation on "
                             f"the given image - {ex}")
 
+    def canny_edge_detection(self, lower_threshold: int, upper_threshold: int, aperture_size: int = None,
+                             l2gradient: bool = None, plot_output: bool = True):
+        """
+        This definition will apply the Canny edge detection method to identify the
+        edges in the image with the specified parameter values.
+
+        Input:
+            lower_threshold -> Lower threshold value in Hysteresis Thresholding. Required parameter
+
+            upper_threshold -> Upper threshold value in Hysteresis Thresholding. Required parameter
+
+            aperture_size -> Aperture size of the Sobel filter. The value should be an odd integer between 3 and 7.
+                If the value is out of range, an error is raised for the cv2 package.
+
+            l2gradient -> Boolean parameter used for more precision in calculating Edge Gradient.
+                Accepted values - True , False
+
+            plot_output -> This is a boolean value which will instruct the program whether to display the
+                        images post pre-processing or not. Will throw value error if value other than the accepted value
+                        passed
+                Accepted values - True , False
+
+        Output:
+            numpy.ndarray -> image post applying the canny edge detection on the given image
+        """
+        try:
+            if type(plot_output) is not bool:
+                raise ValueError(
+                    f"plot_output parameter takes only True or False boolean value. No other values allowed")
+
+            validate_param_type('aperture_size', aperture_size, type(aperture_size), int)
+
+            validate_param_type('L2gradient', l2gradient, type(l2gradient), bool)
+
+            if l2gradient is not None and aperture_size is not None:
+                edge_detail = cv2.Canny(self.image, lower_threshold, upper_threshold, apertureSize=aperture_size,
+                                  L2gradient=l2gradient)
+
+            elif aperture_size is not None:
+                edge_detail = cv2.Canny(self.image, lower_threshold, upper_threshold, apertureSize=aperture_size)
+
+            elif l2gradient is not None:
+                edge_detail = cv2.Canny(self.image, lower_threshold, upper_threshold, L2gradient=l2gradient)
+
+            else:
+                edge_detail = cv2.Canny(self.image, lower_threshold, upper_threshold)
+
+            if plot_output:
+                plot_graph(self.image, edge_detail, self.is_color_image, f'Canny Edge Detection',
+                           is_edge_detection=True)
+
+            return edge_detail
+
+        except ValueError as ex:
+            raise ValueError(ex)
+
+        except CustomException as ex:
+            raise CustomException(ex)
+
+        except Exception as ex:
+            raise Exception(f"An error occurred while trying to apply the Canny Edge detection Algorithm on "
+                            f"the given image - {ex}")

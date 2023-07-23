42f7f839186f133581495e14d6c8e0efa884df2c
thivagar-manickam <thivagar200@gmail.com>
2023-07-23 11:16:02 +0530

    Addition of Sharpening Filter for edge detection

--- basic_image_preprocessing/__init__.py
+++ basic_image_preprocessing/__init__.py
@@ -1,5 +1,9 @@
 from basic_image_preprocessing.image_enhancement.traditional.traditional import TraditionalImageEnhancement
 from basic_image_preprocessing.image_enhancement.conventional.conventional import ConventionalImageEnhancement
+from basic_image_preprocessing.image_enhancement.edge_detection.edge_detection import ImageEdgeDetection
+
 __all__ = [
-    TraditionalImageEnhancement,ConventionalImageEnhancement
+    TraditionalImageEnhancement,
+    ConventionalImageEnhancement,
+    ImageEdgeDetection
 ]
--- basic_image_preprocessing/utility/utlity.py
+++ basic_image_preprocessing/utility/utlity.py
@@ -1,12 +1,12 @@
 import os
 import cv2
-import numpy
+import numpy as np
 from basic_image_preprocessing.exception.custom_exception import CustomException
 import matplotlib.pyplot as plt
 from typing import List, Tuple
 
 
-def load_image(image_path: str, cmap: str) -> Tuple[numpy.ndarray, bool]:
+def load_image(image_path: str, cmap: str) -> Tuple[np.ndarray, bool]:
     """
     load_image -> The definition will be used to load the image from the
     file path and return the loaded image to the calling definition.
@@ -136,3 +136,77 @@ def validate_param_type(param_name, param_value, param_type, expected_type):
     if param_value is not None:
         if param_type != expected_type:
             raise ValueError(f"{param_name} is expected to be in {expected_type} but received value is {param_type}")
+
+
+def create_kernel_mask(kernel_size: int, kernel_type: str, custom_edge_kernel: np.ndarray = None,
+                       normalize: bool = False):
+    """
+    Create a kernel mask for image processing based on the kernel_type passed.
+
+    Parameters:
+        kernel_size (int): The size of the kernel mask (should be odd).
+        kernel_type (str): The type of kernel to create.
+
+                           Possible values: 'identity', 'box', 'gaussian', 'sharpen', 'edge_detection', 'custom'.
+                           Default is 'identity'.
+        normalize (bool): Whether to normalize the kernel or not.
+                          Default is False.
+
+        custom_edge_kernel (np.ndarray): A custom edge detection kernel provided by the user.
+                                         It should be a 2D square matrix ndArray element which has the
+                                         rows and columns value equivalent to the size parameter.
+                                         Mandatory when the kernel_type is 'custom'
+                                         Default is None.
+
+    Returns:
+        np.ndarray: The returned kernel mask is a 2D numpy array.
+    """
+    kernel = None
+    if kernel_size % 2 == 0 or kernel_size <= 0:
+        raise ValueError("Size of the kernel mask should be a positive odd integer.")
+
+    if custom_edge_kernel is not None:
+        if custom_edge_kernel.shape[0] != kernel_size or custom_edge_kernel.shape[1] != kernel_size:
+            raise ValueError("The rows and columns for the custom kernel array should be equal to the specified"
+                             " kernel size")
+
+    if kernel_type not in ['identity', 'box', 'gaussian', 'sharpen', 'edge_detection', 'custom']:
+        raise ValueError(
+            "Invalid kernel_type. Supported Values are: 'identity', 'box', 'gaussian', "
+            "'sharpen', 'edge_detection', 'custom'")
+
+    if kernel_type == 'identity':
+        kernel = np.zeros((kernel_size, kernel_size), dtype=np.float32)
+        center = kernel_size // 2
+        kernel[center, center] = 1.0
+
+    elif kernel_type == 'box':
+        kernel = np.ones((kernel_size, kernel_size), dtype=np.float32)
+        if normalize:
+            kernel /= kernel_size ** 2
+
+    elif kernel_type == 'gaussian':
+        sigma = kernel_size / 6  # You can adjust the sigma value for different levels of blur.
+        x, y = np.meshgrid(np.linspace(-kernel_size // 2, kernel_size // 2, kernel_size),
+                           np.linspace(-kernel_size // 2, kernel_size // 2, kernel_size))
+        kernel = np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+        if normalize:
+            kernel /= kernel.sum()
+
+    elif kernel_type == 'sharpen':
+        kernel = -np.ones((kernel_size, kernel_size), dtype=np.float32)
+        center = kernel_size // 2
+        kernel[center, center] = kernel_size * kernel_size
+
+    elif kernel_type == 'edge_detection':
+        # Default edge detection kernel (laplacian operator)
+        kernel = -np.ones((kernel_size, kernel_size), dtype=np.float32)
+        center = kernel_size // 2
+        kernel[center, center] = kernel_size * kernel_size - 1
+
+    elif kernel_type == 'custom':
+        if custom_edge_kernel is None:
+            raise ValueError("Custom edge detection kernel is required for 'custom' kernel_type.")
+        kernel = custom_edge_kernel
+
+    return kernel

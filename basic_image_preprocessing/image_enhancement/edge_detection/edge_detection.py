04200c0f2467d498e35a5d6a2c5ad77ce42396d5
saivijaayvk <saischn74188@gmail.com>
2023-07-25 10:55:53 +0530

    PR issues rectified for all the methods and checked with manual test cases

--- basic_image_preprocessing/image_enhancement/edge_detection/edge_detection.py
+++ basic_image_preprocessing/image_enhancement/edge_detection/edge_detection.py
@@ -49,6 +49,10 @@ class ImageEdgeDetection:
                 raise ValueError(
                     f"plot_output parameter takes only True or False boolean value. No other values allowed")
 
+            validate_param_type('kernel', kernel, type(kernel), int)
+
+            validate_param_type('smoothness', smoothness, type(smoothness), bool)
+
             image = self.image
             if self.is_color_image:
                 image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)

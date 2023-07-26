5f9057eac90ca1bb7bcb3641ec81cd9a98070363
saivijaayvk <saischn74188@gmail.com>
2023-07-26 20:50:52 +0530

    bilateral filter completed and tested.

--- basic_image_preprocessing/noise_filtering/spatial_filtering/spatial_filtering.py
+++ basic_image_preprocessing/noise_filtering/spatial_filtering/spatial_filtering.py
@@ -1,10 +1,11 @@
-import numpy
+
 import numpy as np
 import cv2
 from warnings import filterwarnings
 from basic_image_preprocessing.exception.custom_exception import CustomException
 from basic_image_preprocessing.utility.utlity import load_image, plot_graph, validate_param_type, \
     create_kernel_mask, validate_param_list_value
+from typing import Union
 
 filterwarnings('ignore')
 
@@ -24,7 +25,7 @@ class SpatialNoiseFiltering:
             raise ex
 
     def noise_filtering(self, kernel_size: int, filter_type: str,
-                        plot_output: bool = True) -> numpy.ndarray:
+                        plot_output: bool = True) -> np.ndarray:
         """
         This definition will apply the noise removal method to identify the
         noises in the image with the specified parameter values.
@@ -73,4 +74,48 @@ class SpatialNoiseFiltering:
 
         except Exception as ex:
             raise Exception(f"An error occurred while trying to apply the {filter_type} mask on "
+                            f"the given image - {ex}")
+
+    def bilateral_filter(self, diameter: int = 5, sigma_color: Union[int, float] = 50,
+                         sigma_space: Union[int, float] = 50, plot_output: bool = True) -> np.ndarray:
+        """
+        A bilateral filter is a non-linear, edge-preserving, and noise-reducing smoothing filter for images.
+    It replaces the intensity of each pixel with a weighted average of intensity values from nearby pixels.
+
+    Input:
+        diameter: Diameter of each pixel neighborhood.
+
+        sigma_color: Value of sigma_color  in the color space.
+        The greater the value, the colors farther to each other will start to get mixed.
+
+        sigma_space:Value of sigma  in the coordinate space.
+        The greater its value, when the pixels are further away it will mix together
+
+        plot_output -> This is a boolean value which will instruct the program whether to display the
+            images post pre-processing or not. Will throw value error if value other than the accepted value
+            passed.
+
+            Accepted values - True , False
+    Output:
+        numpy.ndarray -> image post applying the canny edge detection on the given image
+        """
+
+        try:
+            validate_param_type('diameter', diameter, type(diameter), int)
+
+            image = cv2.bilateralFilter(self.image, diameter, sigma_color, sigma_space)
+
+            if plot_output:
+                plot_graph(self.image, image, self.is_color_image, f'Bilateral')
+
+            return image
+
+        except ValueError as ex:
+            raise ValueError(ex)
+
+        except CustomException as ex:
+            raise CustomException(ex)
+
+        except Exception as ex:
+            raise Exception(f"An error occurred while trying to apply the Bilateral mask on "
                             f"the given image - {ex}")
